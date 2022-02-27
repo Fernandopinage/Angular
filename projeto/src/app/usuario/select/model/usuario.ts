866911215a5d09c6b562337785063c458e7b097c
Luiz Fernando Pinagé Coutinho <luizfernandoluck@hotmail.com>
2022-02-27 16:17:06 -0300

    ok

---
+++ projeto/src/app/usuario/select/model/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+
+    id: number,
+    nome: string,
+    sexo: string,
+    idade: number
+
+
+}
--- projeto/src/app/usuario/select/select.module.ts
+++ projeto/src/app/usuario/select/select.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SelectRoutingModule } from './select-routing.module';
 import { SelectComponent } from './select/select.component';
-
-
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     SelectComponent
   ],
   imports: [
     CommonModule,
-    SelectRoutingModule
+    SelectRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class SelectModule { }
