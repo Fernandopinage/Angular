dbb403caefc2512765c631d7459198bef1efc341
Luiz Fernando Pinagé Coutinho <luizfernandoluck@hotmail.com>
2022-03-01 13:21:27 -0300

    OK

--- projeto/src/app/usuario/select/model/usuario.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuario {
-
-    id: number,
-    nome: string,
-    sexo: string,
-    idade: number
-
-
-}
--- projeto/src/app/usuario/select/select/select.component.ts
+++ projeto/src/app/usuario/select/select/select.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../model/usuario';
+
 
 @Component({
   selector: 'app-select',
@@ -9,16 +9,6 @@ import { Usuario } from '../model/usuario';
 export class SelectComponent implements OnInit {
 
 
-  usuario: Usuario[] = [
-
-    {id:1, nome:'luiz fernando pinage coutinho',sexo:'m',idade:32},
-    {id:2, nome:'paulo victor pinage coutinho',sexo:'m',idade:32},
-    {id:3, nome:'marcus vinicius coutinho',sexo:'m',idade:65},
-    {id:4, nome:'barbara veronica evangelista pinage',sexo:'f',idade:70},
-    {id:5, nome:'supino pipico pinage coutinho',sexo:'f',idade:7},
-  ];
-
-  displayedColumns = ['id', 'nome', 'sexo', 'idade'];
 
   constructor() { }
 
